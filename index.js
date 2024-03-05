63bf5846bc1e376381f042afd1aaec7bb1131c40
nitish-mani <nitishmani63@gnail.com>
2024-03-05 23:02:11 +0530

    forth

--- index.js
+++ index.js
@@ -45,6 +45,10 @@ app.use((error, req, res, next) => {
   res.status(status).json({ message: message, data: data });
 });
 
+app.use("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
 async function main() {
   await mongoose.connect(MONGODB_URI);
 }
